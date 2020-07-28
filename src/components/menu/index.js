4d02cd313f0b928996c1943bcd4f9babcebbe71c
LauraEllen17 <laura.ellen@inovafarm.com.br>
2020-07-27 23:58:46 -0300

    add links

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Logo from '../../assets/img/LogoMain.png';
 import './Menu.css';
-import ButtonLink from './components/ButtonLink';
+//import ButtonLink from './components/ButtonLink';
+import Button from '../../components/Button';
 
 function Menu() {
     return (
@@ -10,9 +11,9 @@ function Menu() {
                 <img className="Logo" src={Logo} alt="Aluraflix logo" />
             </a>
 
-            <ButtonLink className="ButtonLink" href="/">
+            <Button as="a" className="ButtonLink" href="/">
                 Novo Video
-            </ButtonLink>
+            </Button>
         </nav>
     );
 }
